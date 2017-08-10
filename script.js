3cdaac3216a9082ce4dfec70fa8b828636590339
algenisromero <algenisromero@gmail.com>
2017-08-09 22:25:32 -0500

    Adding JS backend functionality

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-var urlStreams = "https://wind-bow.gomix.me/twitch-api/streams/ESL_SC2";
+var urlStreams = "https://wind-bow.gomix.me/twitch-api/streams/freecodecamp";
 
 var ttvRequest = new XMLHttpRequest();
+ttvRequest.open('GET', urlStreams, true);
+ttvRequest.send();
 
-ttvRequest.open('GET', urlStreams);
-ttvRequest.send('');
-
-console.log(ttvRequest);
+ttvRequest.onload = function() {
+    if (ttvRequest.readyState === ttvRequest.DONE) {
+        if (ttvRequest.status === 200) {
+            document.getElementById('here').innerHTML = '<div class="col-md-12">' + ttvRequest.responseText + '</div>';
+        }
+    }
+};
