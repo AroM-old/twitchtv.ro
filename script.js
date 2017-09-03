7b3014fb75abc13887d8fd8cd9728088e8919fdf
algenisromero <algenisromero@gmail.com>
2017-09-03 11:21:22 -0500

    Create listener for buttons

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-// Channels to test
+var channels = [
+    "ESL_SC2",
+    "OgamingSC2",
+    "cretetion",
+    "freecodecamp",
+    "storbeck",
+    "habathcx",
+    "RobotCaleb",
+    "noobs2ninjas"
+];
 
-var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-
-
-
-/* Request to the server and display raw data in screen */
-var urlStreams = "https://wind-bow.gomix.me/twitch-api/streams/ESL_SC2";
-var ttvRequest = new XMLHttpRequest();
-ttvRequest.open('GET', urlStreams, true);
-// ttvRequest.setRequestHeader("cache-control", "no-cache");
-ttvRequest.send(null);
-ttvRequest.onload = function() {
-    if (ttvRequest.readyState === ttvRequest.DONE) {
-        if (ttvRequest.status === 200) {
-            ttvRequest = JSON.parse(this.responseText);
-
-
-            document.getElementById('here').innerHTML = '<div class="col-md-4">' + ttvRequest.stream.channel.name + '</div>';
-        }
+// Button handling
+document.getElementById("all").addEventListener("click", function(){
+    for (var i = 0; i < channels.length; i++) {
+        httpRequest("https://wind-bow.gomix.me/twitch-api/streams/" + channels[i]);
+        document.getElementById("name").innerHTML += "<p>" + channels[i] + "</p>";
+        // console.log(channels[i]);
     }
-};
+        
+});
+
+// Twitch API http request
+function httpRequest(url){
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+            var data = JSON.parse(request.responseText);
+            if (data.stream === null) {
+                document.getElementById("status").innerHTML += "<p>Channel no streaming</p>";
+            }else{
+                document.getElementById("status").innerHTML += "<p>Streaming</p>";
+            }
+            
+        }
+    };
+    request.send();
+}
